ed3027341480780edd9340c837fcac9898780632
e6654321 <enespra@gmail.com>
2024-03-25 04:50:20 +0800

    [Misc] Added compiled subcontracts to the main contract and lessen deploy fee

--- scripts/deploySignProtocol.ts
+++ scripts/deploySignProtocol.ts
@@ -1,29 +1,32 @@
-import { Address, Slice, beginCell, toNano } from '@ton/core';
+import { Address, beginCell, toNano } from '@ton/core';
 import { SignProtocol } from '../wrappers/SignProtocol';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-  const cell = beginCell()
+  const cellAddress = beginCell()
     .storeAddress(Address.parse(process.env.ADDRESS ?? ''))
     .endCell();
+  const attestationCode = await compile('Attestation');
+  const schemaCode = await compile('Schema');
+  const signProtocolCode = await compile('SignProtocol');
   const signProtocol = provider.open(
     SignProtocol.createFromConfig(
       {
-        adminAddress: cell.asSlice(),
+        adminAddress: cellAddress.asSlice(),
         version: 1,
         paused: false,
         schemaCounter: 0,
         attestationCounter: 0,
         initialSchemaCounter: 0,
         initialAttestationCounter: 0,
-        attestationCode: beginCell().endCell(),
-        schemaCode: beginCell().endCell(),
+        attestationCode,
+        schemaCode,
       },
-      await compile('SignProtocol'),
+      signProtocolCode,
     ),
   );
 
-  await signProtocol.sendDeploy(provider.sender(), toNano('0.05'));
+  await signProtocol.sendDeploy(provider.sender(), toNano('0.02'));
 
   await provider.waitForDeploy(signProtocol.address);
 
